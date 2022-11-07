95223910f1be09ac41235ceb7768c91987e5968d
passasooz <francescopassanante@gmail.com>
2022-11-07 09:25:59 +0100

    fix login and signup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,8 +1,12 @@
 const uuid = require("uuid");
 const { validationResult } = require("express-validator");
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 const HttpError = require("../models/http-error");
 
+/*
 const DUMMY_USERS = [
   {
     id: "u1",
@@ -11,48 +15,71 @@ const DUMMY_USERS = [
     password: "123456",
   },
 ];
+*/
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const users = async () => {
+  return await prisma.user.findMany()
+}
+
+const getUsers = async (req, res, next) => {
+  //res.json({ users: DUMMY_USERS });
+  res.json({ users: users });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
+  //const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
+  console.log(users)
+  //const identifiedUser = users.find((user) => user.email === email);
+  const identifiedUser = await prisma.user.findFirst({
+    where: { email : email }
+  })
   if (
-    !identifiedUser ||
-    (identifiedUser && identifiedUser.password !== password)
+    identifiedUser == null ||
+    (typeof identifiedUser === 'object' && identifiedUser.password !== password)
   ) {
     throw new HttpError(
       "Could not identify user, credentials seem to be wrong",
       401
     );
   }
-  res.json({ message: "logged In" });
+  res.json({ 
+    user: identifiedUser, 
+    message: "logged In" 
+  });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed, please check you data", 422);
   }
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((user) => user.email === email);
+  //const hasUser = DUMMY_USERS.find((user) => user.email === email);
+  //const hasUser = users.find((user) => user.email === email);
+  const hasUser = await prisma.user.findFirst({
+    where: { email : email }
+  })
 
-  if (hasUser) {
+  if (typeof hasUser === 'object' && hasUser.email === email) {
     throw new HttpError("Could not create user, email already exists", 422);
   }
 
   const createdUser = {
-    id: uuid.v4(),
-    name,
-    email,
-    password,
+    name: name,
+    email: email,
+    password: password,
   };
 
-  DUMMY_USERS.push(createdUser);
-  res.status(201);
+  //DUMMY_USERS.push(createdUser);
+  const user_created = await prisma.user.create({
+    data: createdUser
+  })
+
+  res.status(201).json({
+    user: user_created
+  });
 };
 
 exports.getUsers = getUsers;
